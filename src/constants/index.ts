a5d139835ecf4df8f3403a8794f0c40400a32f63
Cristian Costa <cristiancosta1991@gmail.com>
2025-06-27 16:18:44 -0300

    Adding testcontainers, jest config, integration test and some constants

---
+++ src/constants/index.ts
@@ -0,0 +1,4 @@
+// Constants.
+import httpStatusCode from './http-status-code';
+
+export { httpStatusCode };
---
+++ test/integration/health.spec.ts
@@ -0,0 +1,53 @@
+import request from 'supertest';
+
+// Types.
+import { TestContext } from './types/setup';
+
+// Setup.
+import { buildResources, teardownResources } from './setup';
+
+// Constants.
+import { httpStatusCode } from '../../src/constants';
+import { HealthResponse } from '../../src/types/health';
+
+jest.setTimeout(30_000);
+
+describe('Health', () => {
+  let context: TestContext;
+
+  beforeAll(async () => {
+    context = await buildResources();
+  });
+
+  afterAll(async () => {
+    await teardownResources(context);
+  });
+
+  describe('GET /health', () => {
+    it('Should return 200 status code and with healthy status and connected database information', async () => {
+      const response = await request(context.app).get('/health');
+      const body = response.body as HealthResponse;
+
+      expect(response.status).toBe(httpStatusCode.OK);
+      expect(body).toHaveProperty('status');
+      expect(body).toHaveProperty('dependencies');
+      expect(body.dependencies).toHaveProperty('database');
+      expect(body.status).toEqual('healthy');
+      expect(body.dependencies.database).toEqual('connected');
+    });
+
+    it('Should return 200 status code and with healthy status and not-connected database information', async () => {
+      await context.dataSource.destroy();
+      const response = await request(context.app).get('/health');
+      const body = response.body as HealthResponse;
+
+      expect(response.status).toBe(httpStatusCode.OK);
+      expect(body).toHaveProperty('status');
+      expect(body).toHaveProperty('dependencies');
+      expect(body.dependencies).toHaveProperty('database');
+      expect(body.dependencies).toHaveProperty('reason');
+      expect(body.status).toEqual('healthy');
+      expect(body.dependencies.database).toEqual('not-connected');
+    });
+  });
+});
